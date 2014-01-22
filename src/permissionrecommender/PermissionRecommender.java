42f25c0cd217a8b933ff48979c002a9e2b552538
touahmed <touahmed@14f1d81e-5b87-4551-8290-e6408f7f0722>
2014-01-22 15:34:35 +0000

    Changed

--- src/permissionrecommender/PermissionRecommender.java
+++ src/permissionrecommender/PermissionRecommender.java
@@ -4,15 +4,56 @@
  */
 package permissionrecommender;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author touahmed
  */
 public class PermissionRecommender {
 
+    public static Vector<Data> allData = new Vector<Data>();
+    ItemNames names= new ItemNames();
     /**
      * @param args the command line arguments
      */
+    
+    public static void readFile(boolean header, String filename)
+    {
+    // Variables
+        BufferedReader datain;
+        String line;
+        
+
+        // Open File
+        try {
+            datain = new BufferedReader(new FileReader(filename));
+        } catch (FileNotFoundException ex) {
+            datain = null;
+            Logger.getLogger(PermissionRecommender.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        // Read The Whole File and Create Dataset
+        try {
+            for (int i = 0; (line=datain.readLine())!= null; i++) {
+              //  System.out.println(line);
+                //skip Header
+                if(header)
+                    
+                if(i!=0)
+                    allData.add(new Data(i, line));
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(PermissionRecommender.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
     public static void main(String[] args) {
         // TODO code application logic here
     }
